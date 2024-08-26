58e8ba7ad2f9ceea1f45436731ed321354badd6b
shubham <shubhamshetkar46@gmail.com>
2024-08-26 22:22:18 +0530

    extra scripts

---
+++ script01.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Description: 
+# Author:
+# Date Created:
+# Version: 
+# Date Modified:
+echo "Hello world! Welcome to Shell classes"
+echo "Hello again! welcome to shell classes"
+echo "hello developers"
+echo "Hello Milton"
